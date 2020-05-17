13dd75c37c308bf4bc37c4bde89d58752d3b59f9
Neroec <61140152+Neroec@users.noreply.github.com>
2020-05-17 05:46:27 +0300

    Update ObjectsCollision.h

--- tests/ObjectsCollision.h
+++ tests/ObjectsCollision.h
@@ -4,7 +4,7 @@
 
 TEST(ObjectsCollision_positive, zero_yVelocity_right_collision)
 {
-    vector<int> objects = {
+    int obj[] = {
         90, 32, 32,
         0, 836, 32, 836, 64, 836, 96, 836, 128, 836, 160, 836, 192, 836, 224, 836, 256, 836, 288, 836,
         320, 836, 352, 836, 384, 836, 416, 836, 448, 836, 480, 836, 512, 836, 544, 836, 576, 836, 608, 836,
@@ -16,17 +16,21 @@ TEST(ObjectsCollision_positive, zero_yVelocity_right_collision)
         1088, 804, 1088, 772, 1088, 740, 1088, 708, 1088, 676, 1088, 644, 1088, 612, 1088, 580, 1088, 548, 1088, 516,
         1088, 484, 1088, 452, 1088, 420, 1088, 388, 1088, 356, 1088, 324, 1088, 292, 1088, 260, 1088, 228, 1088, 196
     };
-
-    vector<int> collis = {
+    
+    vector<int> objects (obj, obj + sizeof(obj) / sizeof(int));  
+        
+    int col[] = {
         321, 718, 0, 0
     };
 
+    vector<int> collis (col, col + sizeof(col) / sizeof(int));
+        
     EXPECT_EQ(objectsCollision(98, 118, 316, 718, -5, 0, objects), collis);
 }
 
 TEST(ObjectsCollision_positive, zero_yVelocity_left_collision)
 {
-    vector<int> objects = {
+    int obj[] = {
         90, 32, 32,
         0, 836, 32, 836, 64, 836, 96, 836, 128, 836, 160, 836, 192, 836, 224, 836, 256, 836, 288, 836,
         320, 836, 352, 836, 384, 836, 416, 836, 448, 836, 480, 836, 512, 836, 544, 836, 576, 836, 608, 836,
@@ -38,17 +42,21 @@ TEST(ObjectsCollision_positive, zero_yVelocity_left_collision)
         1088, 804, 1088, 772, 1088, 740, 1088, 708, 1088, 676, 1088, 644, 1088, 612, 1088, 580, 1088, 548, 1088, 516,
         1088, 484, 1088, 452, 1088, 420, 1088, 388, 1088, 356, 1088, 324, 1088, 292, 1088, 260, 1088, 228, 1088, 196
     };
+    
+    vector<int> objects (obj, obj + sizeof(obj) / sizeof(int));   
 
-    vector<int> collis = {
+    int col[] = {
         989, 718, 0, 0
     };
+        
+    vector<int> collis (col, col + sizeof(col) / sizeof(int));  
 
     EXPECT_EQ(objectsCollision(98, 118, 994, 718, 5, 0, objects), collis);
 }
 
 TEST(ObjectsCollision_positive, not_zero_yVelocity_right_collision)
 {
-    vector<int> objects = {
+    int obj[] = {
         90, 32, 32,
         0, 836, 32, 836, 64, 836, 96, 836, 128, 836, 160, 836, 192, 836, 224, 836, 256, 836, 288, 836,
         320, 836, 352, 836, 384, 836, 416, 836, 448, 836, 480, 836, 512, 836, 544, 836, 576, 836, 608, 836,
@@ -60,17 +68,21 @@ TEST(ObjectsCollision_positive, not_zero_yVelocity_right_collision)
         1088, 804, 1088, 772, 1088, 740, 1088, 708, 1088, 676, 1088, 644, 1088, 612, 1088, 580, 1088, 548, 1088, 516,
         1088, 484, 1088, 452, 1088, 420, 1088, 388, 1088, 356, 1088, 324, 1088, 292, 1088, 260, 1088, 228, 1088, 196
     };
+    
+    vector<int> objects (obj, obj + sizeof(obj) / sizeof(int));
 
-    vector<int> collis = {
+    int col[] = {
         321, 718, 0, 1
     };
+        
+    vector<int> collis (col, col + sizeof(col) / sizeof(int));   
 
     EXPECT_EQ(objectsCollision(98, 118, 316, 719, -5, 1, objects), collis);
 }
 
 TEST(ObjectsCollision_positive, not_zero_yVelocity_left_collision)
 {
-    vector<int> objects = {
+    int obj[] = {
         90, 32, 32,
         0, 836, 32, 836, 64, 836, 96, 836, 128, 836, 160, 836, 192, 836, 224, 836, 256, 836, 288, 836,
         320, 836, 352, 836, 384, 836, 416, 836, 448, 836, 480, 836, 512, 836, 544, 836, 576, 836, 608, 836,
@@ -82,17 +94,21 @@ TEST(ObjectsCollision_positive, not_zero_yVelocity_left_collision)
         1088, 804, 1088, 772, 1088, 740, 1088, 708, 1088, 676, 1088, 644, 1088, 612, 1088, 580, 1088, 548, 1088, 516,
         1088, 484, 1088, 452, 1088, 420, 1088, 388, 1088, 356, 1088, 324, 1088, 292, 1088, 260, 1088, 228, 1088, 196
     };
-
-    vector<int> collis = {
+    
+    vector<int> objects (obj, obj + sizeof(obj) / sizeof(int));
+        
+    int col[] = {
         989, 718, 0, 0
     };
+
+    vector<int> collis (col, col + sizeof(col) / sizeof(int)); 
 
     EXPECT_EQ(objectsCollision(98, 118, 994, 719, 5, 1, objects), collis);
 }
 
 TEST(ObjectsCollision_positive, up_collision)
 {
-    vector<int> objects = {
+    int obj[] = {
         90, 32, 32,
         0, 836, 32, 836, 64, 836, 96, 836, 128, 836, 160, 836, 192, 836, 224, 836, 256, 836, 288, 836,
         320, 836, 352, 836, 384, 836, 416, 836, 448, 836, 480, 836, 512, 836, 544, 836, 576, 836, 608, 836,
@@ -104,17 +120,21 @@ TEST(ObjectsCollision_positive, up_collision)
         1088, 804, 1088, 772, 1088, 740, 1088, 708, 1088, 676, 1088, 644, 1088, 612, 1088, 580, 1088, 548, 1088, 516,
         1088, 484, 1088, 452, 1088, 420, 1088, 388, 1088, 356, 1088, 324, 1088, 292, 1088, 260, 1088, 228, 1088, 196
     };
-
-    vector<int> collis = {
+    
+    vector<int> objects (obj, obj + sizeof(obj) / sizeof(int));   
+        
+    int col[] = {
         666, 718, 0, 1
     };
+
+    vector<int> collis (col, col + sizeof(col) / sizeof(int));  
 
     EXPECT_EQ(objectsCollision(98, 118, 666, 719, 0, 1, objects), collis);
 }
 
 TEST(ObjectsCollision_positive, down_collision)
 {
-    vector<int> objects = {
+    int obj[] = {
         100, 32, 32,
         0, 600, 32, 600, 64, 600, 96, 600, 128, 600, 160, 600, 192, 600, 224, 600, 256, 600, 288, 600,
         320, 600, 352, 600, 384, 600, 416, 600, 448, 600, 480, 600, 512, 600, 544, 600, 576, 600, 608, 600,
@@ -127,17 +147,21 @@ TEST(ObjectsCollision_positive, down_collision)
         960, 836, 992, 836, 1024, 836, 1056, 836, 1088, 836, 1120, 836, 1152, 836, 1184, 836, 1216, 836, 1248, 836,
         1280, 836, 1312, 836, 1344, 836, 1376, 836, 1408, 836, 1440, 836, 1472, 836, 1504, 836, 1536, 836, 1568, 836
     };
-
-    vector<int> collis = {
+    
+    vector<int> objects (obj, obj + sizeof(obj) / sizeof(int));  
+        
+    int col[] = {
         791, 632, 0, 2
     };
+
+    vector<int> collis (col, col + sizeof(col) / sizeof(int));
 
     EXPECT_EQ(objectsCollision(98, 118, 791, 631, 0, -28, objects), collis);
 }
 
 TEST(ObjectsCollision_positive, inside_collision)
 {
-    vector<int> objects = {
+    int obj[] = {
         70, 32, 32,
         0, 836, 32, 836, 64, 836, 96, 836, 128, 836, 160, 836, 192, 836, 224, 836, 256, 836, 288, 836,
         320, 836, 352, 836, 384, 836, 416, 836, 448, 836, 480, 836, 512, 836, 544, 836, 576, 836, 608, 836,
@@ -147,17 +171,21 @@ TEST(ObjectsCollision_positive, inside_collision)
         1260, 580, 1260, 548, 1260, 516, 1260, 484, 1260, 452, 1260, 420, 1260, 388, 1260, 356, 1260, 324, 1260, 292,
         1100, 600, 1132, 600, 1164, 600, 1196, 600, 1228, 600, 1260, 600, 1292, 600, 1324, 600, 1356, 600, 1388, 600
     };
-
-    vector<int> collis = {
+    
+    vector<int> objects (obj, obj + sizeof(obj) / sizeof(int));  
+        
+    int col[] = {
         1156, 482, 0, 1
     };
+
+    vector<int> collis (col, col + sizeof(col) / sizeof(int));  
 
     EXPECT_EQ(objectsCollision(98, 118, 1166, 483, 5, 1, objects), collis);
 }
 
 TEST(ObjectsCollision_negative, sizes_not_more_0)
 {
-    vector<int> objects = {
+    int obj[] = {
         70, -32, -32,
         0, 836, 32, 836, 64, 836, 96, 836, 128, 836, 160, 836, 192, 836, 224, 836, 256, 836, 288, 836,
         320, 836, 352, 836, 384, 836, 416, 836, 448, 836, 480, 836, 512, 836, 544, 836, 576, 836, 608, 836,
@@ -167,10 +195,14 @@ TEST(ObjectsCollision_negative, sizes_not_more_0)
         1260, 580, 1260, 548, 1260, 516, 1260, 484, 1260, 452, 1260, 420, 1260, 388, 1260, 356, 1260, 324, 1260, 292,
         1100, 600, 1132, 600, 1164, 600, 1196, 600, 1228, 600, 1260, 600, 1292, 600, 1324, 600, 1356, 600, 1388, 600
     };
-
-    vector<int> collis = {
+    
+    vector<int> objects (obj, obj + sizeof(obj) / sizeof(int));
+        
+    int col[] = {
         1166, 483, -1, 0
     };
+
+    vector<int> collis (col, col + sizeof(col) / sizeof(int));  
 
     EXPECT_EQ(objectsCollision(-98, -118, 1166, 483, -5, -1, objects), collis);
 }
