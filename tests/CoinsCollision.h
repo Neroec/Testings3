a61b3f2c8b7efbb921dde7d769ffddded661d504
Neroec <61140152+Neroec@users.noreply.github.com>
2020-05-17 05:52:23 +0300

    Update CoinsCollision.h

--- tests/CoinsCollision.h
+++ tests/CoinsCollision.h
@@ -4,75 +4,95 @@
 
 TEST(CoinsCollision_positive, single_touched)
 {
-    vector<int> oldCoins = {
-      3, 10, 10,
+    int old[] = {
+        3, 10, 10,
       10, 10, 100, 100, 200, 200
     };
-
-    vector<int> newCoins = {
+    
+    vector<int> oldCoins (old, old + sizeof(old) / sizeof(int));
+      
+    int new[] = {
         2, 10, 10,
         100, 100, 200, 200
     };
+
+    vector<int> newCoins (new, new + sizeof(new) / sizeof(int));  
 
     EXPECT_EQ(coinsCollision(10, 10, 1, 1, oldCoins), newCoins);
 }
 
 TEST(CoinsCollision_positive, multiple_touched_nearby)
 {
-    vector<int> oldCoins = {
-      3, 10, 10,
+    int old[] = {
+        3, 10, 10,
       10, 10, 10, 20, 200, 200
     };
-
-    vector<int> newCoins = {
+    
+    vector<int> oldCoins (old, old + sizeof(old) / sizeof(int));
+        
+    int new[] = {
         1, 10, 10,
         200, 200
     };
+
+    vector<int> newCoins (new, new + sizeof(new) / sizeof(int));  
 
     EXPECT_EQ(coinsCollision(10, 10, 5, 15, oldCoins), newCoins);
 }
 
 TEST(CoinsCollision_positive, multiple_touched_apart)
 {
-    vector<int> oldCoins = {
-      3, 10, 10,
+    int old[] = {
+        3, 10, 10,
       10, 10, 200, 200, 10, 20
     };
-
-    vector<int> newCoins = {
+    
+    vector<int> oldCoins (old, old + sizeof(old) / sizeof(int)); 
+        
+    int new[] = {
         1, 10, 10,
         200, 200
     };
+
+    vector<int> newCoins (new, new + sizeof(new) / sizeof(int));  
 
     EXPECT_EQ(coinsCollision(10, 10, 5, 15, oldCoins), newCoins);
 }
 
 TEST(CoinsCollision_positive, not_touched)
 {
-    vector<int> oldCoins = {
-      3, 10, 10,
+    int old[] = {
+        3, 10, 10,
       10, 10, 100, 100, 200, 200
     };
-
-    vector<int> newCoins = {
+    
+    vector<int> oldCoins (old, old + sizeof(old) / sizeof(int)); 
+        
+    int new[] = {
         3, 10, 10,
         10, 10, 100, 100, 200, 200
     };
+
+    vector<int> newCoins (new, new + sizeof(new) / sizeof(int));   
 
     EXPECT_EQ(coinsCollision(10, 10, 0, 0, oldCoins), newCoins);
 }
 
 TEST(CoinsCollision_negative, sizes_not_more_0)
 {
-    vector<int> oldCoins = {
-      3, -10, -10,
+    int old[] = {
+        3, -10, -10,
       10, 10, 100, 100, 200, 200
     };
-
-    vector<int> newCoins = {
+    
+    vector<int> oldCoins (old, old + sizeof(old) / sizeof(int));
+        
+    int new[] = {
         3, -10, -10,
         10, 10, 100, 100, 200, 200
       };
 
+    vector<int> newCoins (new, new + sizeof(new) / sizeof(int));
+        
     EXPECT_EQ(coinsCollision(-10, -10, 5, 15, oldCoins), newCoins);
 }
